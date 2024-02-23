0d345e75d58e29777eab67390bf5097674ff6137
Youssefproof771 <youssefproof@gmail.com>
2024-02-23 09:45:40 +0200

    ay 7aga

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int health;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Allybullet")) 
+        {
+            Destroy(collision.gameObject);
+            health -= 10;
+            if (health <= 0)
+            {
+                StartCoroutine("EnemyDies");
+            }
+        }
+    }
+    IEnumerator EnemyDies()
+    {
+        animator.SetBool("Dies", true);
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/Playerhealth.cs
+++ Assets/Scripts/Player/Playerhealth.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Playerhealth : MonoBehaviour
 {
     public int health;
-
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +17,13 @@ public class Playerhealth : MonoBehaviour
     {
         
     }
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Vector2 dir = (transform.position - collision.transform.position).normalized;
+            rb.AddForce (dir*10,ForceMode2D.Impulse);
+            health -= 15;
+        }
+    }
 }
